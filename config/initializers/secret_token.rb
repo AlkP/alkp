a1a897de11ebd827d8bf6fb09babd7eef4cdf13e
alkp <apasenko@gmail.com>
2013-10-15 15:53:50 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alkp::Application.config.secret_key_base = 'e254d27d477782796446b820198ceb84d100f733756d6979f3c43e9abd967e0fe3aa912b75b0bb6aceee17d5dfe36330e7b6ead5ed4d7020d251521a612f4cc3'
